b8d54d526a95524281fb265ced1a69a95be491e4
irfanyadi <irfanyadi10@gmail.com>
2021-10-21 14:53:06 +0700

    Validation

--- weblanjut/app/Views/posts/create.php
+++ weblanjut/app/Views/posts/create.php
@@ -220,25 +220,45 @@
                 Form Tambah Posts
             </div>
             <div class="card-body">
-                <form action="/admin/posts/store" method="post">
+                <form action="/admin/posts/store" method="POST">
                     <div class="row">
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label for="judul">Judul Postingan</label>
-                                <input type="text" class="form-control" id="judul" name="judul">
+                                <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' :  ''; ?>"  id="judul" name="judul" value="<?= old('judul'); ?>">
+                                <?php if ($validation->hasError('judul')) : ?>
+                                  <div class="invalid-feedback">
+                                    <?= $validation->getError('judul'); ?>
+                                  </div>
+                                <?php endif; ?>
                             </div>
                             <div class="form-group">
                                 <label for="slug">slug</label>
-                                <input type="text" class="form-control" id="slug" name="slug">
+                                <input type="text" class="form-control <?= ($validation->hasError('slug')) ? 'is-invalid' :  ''; ?>"  id="slug" name="slug" value="<?= old('slug'); ?>">
+                                <?php if ($validation->hasError('slug')) : ?>
+                                  <div class="invalid-feedback">
+                                    <?= $validation->getError('slug'); ?>
+                                  </div>
+                                <?php endif; ?>
                             </div>
                             <div class="form-group">
                                 <label for="kategori">Kategori Postingan</label>
-                                <input type="text" class="form-control" id="kategori" name="kategori">
-                            </div>
+                                <input type="text" class="form-control <?= ($validation->hasError('kategori')) ? 'is-invalid' :  ''; ?>"  id="kategori" name="kategori" value="<?= old('kategori'); ?>">
+                                <?php if ($validation->hasError('kategori')) : ?>
+                                  <div class="invalid-feedback">
+                                    <?= $validation->getError('kategori'); ?>
+                                  </div>
+                                <?php endif; ?>
+                              </div>
                             <div class="form-group">
                                 <label for="author">Author</label>
-                                <input type="text" class="form-control" id="author" name="author">
-                            </div>
+                                <input type="text" class="form-control <?= ($validation->hasError('author')) ? 'is-invalid' :  ''; ?>"  id="author" name="author" value="<?= old('author'); ?>">
+                                <?php if ($validation->hasError('author')) : ?>
+                                  <div class="invalid-feedback">
+                                    <?= $validation->getError('author'); ?>
+                                  </div>
+                                <?php endif; ?>
+                              </div>
                         </div>
                         <div class="col-md-8">
                             <label for="deskripsi">Deskripsi Postingan</label>
